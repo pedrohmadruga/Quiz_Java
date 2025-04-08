d16622e392fc7783ee3853eccd17b4ae92921935
pedrohmadruga <pedrohfonseca01@gmail.com>
2025-04-08 18:51:49 -0300

    Adicionando serial à LineNotFoundException

--- src/models/exceptions/LineNotFoundException.java
+++ src/models/exceptions/LineNotFoundException.java
@@ -1,6 +1,8 @@
 package models.exceptions;
 
 public class LineNotFoundException extends Exception {
+	private static final long serialVersionUID = 1L;
+
 	public LineNotFoundException(String msg) {
 		super(msg);
 	}
